c5a4e69cb12b176b3d68391f52225d8a719d1bc4
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-08-07 22:05:46 +0900

    ViteからLaravelMixに変更

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tubuyaku</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <script src="{{ mix('/js/app.js') }}"></script>
 </head>
 
 <body>
