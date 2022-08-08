07c51ac8845dc4937f285e8849252f10d22a2302
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-08-08 20:45:16 +0900

    編集と削除コンポーネント作成

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Tubuyaku' }}</title>
+    @stack('css')
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}"></script>
 </head>
